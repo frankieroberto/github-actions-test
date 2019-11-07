04e9af25ab4f7a64d9f18c9caedb0978d5367b2a
Frankie Roberto <frankie@frankieroberto.com>
2019-11-07 10:38:49 +0000

    Update test_spec.rb

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,5 +1,7 @@
-RSpec.describe "Test" do
-  it "tests the tests" do
-    expect("test").to eql("test")
+# frozen_string_literal: true
+
+RSpec.describe 'Test' do
+  it 'tests the tests' do
+    expect('test').to eql('test')
   end
 end
